42d1269d05c9f38f7cc1e679a744c7ebff93c655
3narinder <narinderd9@gmail.com>
2024-05-03 17:43:16 +0530

    work on button props and varients

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,6 +1,10 @@
+import { cva } from "class-variance-authority";
 import { ComponentProps } from "react";
+
+const buttonStyles = cva("w-full", "rounded-md", "fomnt");
+
 type ButtonProps = ComponentProps<"button">;
 
-export const Button = ({...props}:ButtonProps) => {
-  return <button className="text-red-400" {...props}/>;
+export const Button = ({ ...props }: ButtonProps) => {
+  return <button className="text-red-400" {...props} />;
 };
